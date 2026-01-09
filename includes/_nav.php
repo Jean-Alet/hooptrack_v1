5db763a8c6d27533e8738b0003b1423407b6470e
JeanAlet <jean.alet@etu.iut-tlse3.fr>
2026-01-09 13:16:16 +0100

    major changes

--- includes/_nav.php
+++ includes/_nav.php
@@ -2,7 +2,7 @@
     <a href="../pages/accueil_disp.php" class="btn"> Accueil</a>
     <a href="../pages/equipe_disp.php" class="btn"> Joueurs</a>
     <a href="../pages/match_disp.php" class="btn"> Matchs</a>
-    <a href="../pages/feuille_match_disp.php" class="btn"> Feuilles de Matchs</a>
+    <a href="../pages/feuille_match_disp.php" class="btn"> Feuilles de match</a>
     <a href="../pages/statistiques_disp.php" class="btn"> Statistiques</a>
     <a href="../core/déconnecter.php" class="btn">Déconnexion</a>
 </nav>
